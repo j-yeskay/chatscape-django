99a1d564c8d1f3505010de4908906c1dff570379
j-yeskay <asksathishk@gmail.com>
2021-07-29 20:09:44 +0530

    hide secret_key and added requirements.txt

--- chatscape/settings.py
+++ chatscape/settings.py
@@ -1,17 +1,23 @@
 from pathlib import Path
 import os
+
+from decouple import config
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pckacyl5&6q@(_x_+m1r!ca(gfoe!hu$-g_doc66&0--pl_9(b'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default = True)
 
 ALLOWED_HOSTS = []
 
